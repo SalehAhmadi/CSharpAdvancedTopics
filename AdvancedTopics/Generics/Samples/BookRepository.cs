900acec17a55014974e8365697e81087e8e38ecf
Saleh Ahmadi <ahmadi.saleh94@gmail.com>
2020-03-23 22:15:05 +0430

    Lambda Expression Added

---
+++ AdvancedTopics/Generics/Samples/BookRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdvancedTopics.Generics.Samples
+{
+    public class BookRepository
+    {
+        public List<Book> GetBooks()
+        {
+            return new List<Book> {
+            new Book(){Title = "t1", Price = 5 },
+            new Book(){Title = "t2", Price = 7 },
+            new Book(){Title = "t3", Price = 43 }
+            };
+        }
+    }
+}
--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -8,6 +8,17 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
+            // =>
+            const int factor = 5;
+            Func<int, int> multipler = n => n * factor;
+            Console.WriteLine(multipler(5));
+
+            var books = new BookRepository().GetBooks();
+            //var cheapBooks = books.FindAll(IsCheaperThan10Dollars);
+            var cheapBooks = books.FindAll(b => b.Price < 10);
+            foreach (var book in cheapBooks)
+                Console.WriteLine(book.Title);
+            //Delegates
             var photo = new PhotoProcessor();
             var filters = new PhotoFilters();
             //PhotoProcessor.PhotoFilterHandler filterHandler = filters.ApplyBrightness;
@@ -15,7 +26,7 @@ namespace TestConsole
             filterHandler += filters.ApplyContrast;
             filterHandler += RemoveRedEyeFilter;
             photo.Process("test", filterHandler);
-
+            //Generics
             var numbers = new Generic<int>();
             numbers.Add(10);
 
@@ -31,6 +42,11 @@ namespace TestConsole
         static void RemoveRedEyeFilter(Photo photo)
         {
             Console.WriteLine("Remove RedEye");
+        }
+
+        static bool IsCheaperThan10Dollars(Book book)
+        {
+            return book.Price < 10;
         }
     }
 }
